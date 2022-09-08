944367de9a24ecf7d6095532f2727535fe9b772e
peter enejo <petstorm101@gmail.com>
2022-09-08 22:15:03 +0100

    commit tree f76ad633589506ced0bca505c84f362c5d6ca9c2

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 2022_09_08_204449) do
 
     create_table "movies", force: :cascade do |t|
-        t.string "title"
-        t.integer "release_date"
-        t.string "director"
-        t.string "lead"
-        t.boolean "in_theaters"
+      t.string "title"
+      t.integer "release_date"
+      t.string "director"
+      t.string "lead"
+      t.boolean "in_theaters"
     end
-    
+  
 end
-    
